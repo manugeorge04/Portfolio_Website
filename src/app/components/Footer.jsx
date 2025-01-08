cee4db2a204529e9bd1ae2801256b1bd95a36a66
manugeorge04 <manugeorge004@gmail.com>
2025-01-08 15:32:37 -0700

    Fixed footer alignment

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,10 +1,12 @@
 
 const Footer = () => {
   return (
-    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
+    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white flex justify-center">
       <div className="container p-12 flex justify-between">
         <span>Manu</span>
-        <p className="text-slate-600">All rights reserved.</p>
+        <p className="text-slate-600">
+          All rights reserved.
+        </p>
       </div>
     </footer>
   );
