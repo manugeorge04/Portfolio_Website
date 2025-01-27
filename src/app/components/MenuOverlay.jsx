bcc8cf870415d595b03d45d885244b3def3e9323
manugeorge04 <manugeorge004@gmail.com>
2025-01-27 01:31:52 -0700

    Close navbar in mobile screens after navigation

--- src/app/components/MenuOverlay.jsx
+++ src/app/components/MenuOverlay.jsx
@@ -1,12 +1,14 @@
-import React from "react";
 import NavLink from "./NavLink";
 
-const MenuOverlay = ({ links }) => {
+const MenuOverlay = ({ links, onClick }) => {
   return (
     <ul className="flex flex-col py-4 items-center">
       {links.map((link, index) => (
-        <li key={index}>
-          <NavLink href={link.path} title={link.title} />
+        <li key={index} onClick={onClick}>
+          <NavLink
+            href={link.path}
+            title={link.title}
+          />
         </li>
       ))}
     </ul>
