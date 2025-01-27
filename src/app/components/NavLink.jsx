0e7345cf1668dcc197ae103f9d5519bb7ae68cae
manugeorge04 <manugeorge004@gmail.com>
2025-01-27 01:31:04 -0700

    Added smooth scroll

--- src/app/components/NavLink.jsx
+++ src/app/components/NavLink.jsx
@@ -1,10 +1,37 @@
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-const NavLink = ({ href, title }) => {
+const NavLink = ({ href, title, className = "" }) => {
+
+  const pathname = usePathname();
+
+  const handleClick = (e) => {
+    if (pathname === "/") {
+      e.preventDefault();
+    }
+    if (href === '/') {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    } else {
+      const targetId = href.replace("/#", "");
+      const targetElement =
+        document.getElementById(targetId);
+      if (targetElement) {
+        targetElement.scrollIntoView({
+          behavior: "smooth",
+        });
+      }
+    }
+    window.history.pushState({}, "", href);
+  };
+
   return (
     <Link
       href={href}
-      className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white"
+      onClick={handleClick}
+      className={`block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white ${className}`}
     >
       {title}
     </Link>
